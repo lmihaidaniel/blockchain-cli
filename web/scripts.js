09227eba189c6fcd59b9da2f250fec2cfeedd12b
Lacatusu Mihai Daniel <lacatusu@adobe.com>
2017-11-15 17:49:00 +0100

    include riot

--- web/scripts.js
+++ web/scripts.js
@@ -3,6 +3,20 @@ socket.on('connect', function(data) {
     socket.emit('join', 'Hello World from client');
 });
 
-socket.on('broadcast', function(data) {
+
+riot.tag('blockchain',
+'<ul><li each={blocks}>{index} - {hash} - {data}</li></ul>',
+'blockchain { display: block; border: 2px }',
+'class="table"',
+function (opts) {
+  var self = this
+  this.blocks = []
+
+  socket.on('broadcast', function(data) {
     console.log(data);
+    self.blocks = data;
+    self.update();
+  });
 });
+
+riot.mount('blockchain');
